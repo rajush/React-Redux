69deecf51d812ad690bc68fc814f122b581e7f49
rajush <rajushshakya@gmail.com>
2017-07-24 23:54:31 -0700

    THUNK - create loadCourses action - Action naming conventions

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -7,7 +7,28 @@
  */
 
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return {  type: types.CREATE_COURSE, course };
+export function loadCoursesSuccess(courses) {
+  return {  type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+/**
+ * THUNK
+ * A thunk always returns a function that accepts a dispatch -- 'return function(dispatch)'. So this wrapper function
+ * will exist in every one of our thunks.
+ * Inside the body of our thunk, it's a pretty logical spot to go ahead and make our API call.
+ */
+export function loadCourses() {
+    return function(dispatch) {
+        return courseApi.getAllCourses().then(courses => {
+          /**
+           * We're ready to dispatch an action creator. So we dispatch something called 'loadCoursesSuccess'.
+           * And we will pass it a list of courses.
+           */
+            dispatch(loadCoursesSuccess(courses));
+        }),catch(error => {
+            throw(error);
+        });
+    }
 }
